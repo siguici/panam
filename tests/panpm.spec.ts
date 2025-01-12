dab9db73c0b35735f56b07cb02d9a12657511b14
Sigui Kessé Emmanuel <siguici@proton.me>
2025-01-12 18:14:57 +0100

    :test_tube: Test the default package manager

---
+++ tests/panpm.spec.ts
@@ -0,0 +1,23 @@
+import path from 'node:path';
+import process from 'node:process';
+import { test } from '@japa/runner';
+import pm from 'panam';
+import whichPm from 'which-pm-runs';
+
+delete process.env.npm_config_user_agent;
+
+const fixturesDir = path.join(import.meta.dirname, 'fixtures');
+const fixtureDir = (pmName: string) => path.join(fixturesDir, pmName);
+
+test(pm.name, async ({ assert }) => {
+  const cwd = fixtureDir(pm.name);
+  process.chdir(cwd);
+
+  const version = await pm.version();
+  assert.isString(version);
+
+  const install = await pm.install({ cwd });
+  const result = await install.result;
+
+  assert.isTrue(result.status);
+}).disableTimeout();
