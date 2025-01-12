80d72f30328743cf48afdc452246bea9f5fa6b39
Sigui Kessé Emmanuel <siguici@proton.me>
2025-01-12 18:58:26 +0100

    :white_check_mark: Test the current package manager name

--- tests/panpm.spec.ts
+++ tests/panpm.spec.ts
@@ -4,10 +4,16 @@ import { test } from '@japa/runner';
 import pm from 'panam';
 import whichPm from 'which-pm-runs';
 
+const currentPm = whichPm()?.name ?? 'npm';
+
 delete process.env.npm_config_user_agent;
 
 const fixturesDir = path.join(import.meta.dirname, 'fixtures');
 const fixtureDir = (pmName: string) => path.join(fixturesDir, pmName);
+
+test(`pm should be ${currentPm}`, ({ assert }) => {
+  assert.equal(pm.name, currentPm);
+});
 
 test(pm.name, async ({ assert }) => {
   const cwd = fixtureDir(pm.name);
