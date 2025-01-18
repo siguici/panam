a02d8df5660bae845a1535bde88d3de8e6ab8960
Sigui Kessé Emmanuel <siguici@proton.me>
2025-01-18 13:10:27 +0100

    :recycle: Move types to utils

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,12 +1,32 @@
 import fs from 'node:fs';
-import type {
-  PackageJsonInfo,
-  PackageManagerInfo,
-  PackageManagerName,
-  RuntimeInfo,
-  RuntimeName,
-  Version
-} from './types';
+
+export type PackageManagerName =
+  | 'npm'
+  | 'cnpm'
+  | 'yarn'
+  | 'pnpm'
+  | 'bun'
+  | 'deno';
+export type RuntimeName = 'node' | 'bun' | 'deno';
+export type Version = `${number}.${number}.${number}` | 'latest';
+
+export interface RuntimeInfo {
+  name: RuntimeName;
+  version?: Version;
+}
+
+export interface PackageManagerInfo {
+  name: PackageManagerName;
+  version?: Version;
+  lockfile?: string;
+}
+
+export interface PackageJsonInfo {
+  name: string;
+  version: string;
+  engines: Record<RuntimeName | PackageManagerName, string>;
+  packageManager: `${PackageManagerName}@${Version}`;
+}
 
 export const defaultRuntimeInfo: RuntimeInfo = {
   name: 'node',
