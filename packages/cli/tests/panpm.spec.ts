5cb57feab9665f7857f7f751c182b02d74e2ebba
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-06-12 16:40:55 +0100

    🔨 Run `tsx` directly

--- packages/cli/tests/panpm.spec.ts
+++ packages/cli/tests/panpm.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@japa/runner';
 import { runtime } from 'panam';
 
 test(runtime.name, async ({ assert }) => {
-  const result = await runtime.run('dist/cli.js exec tsx tests/hello.ts');
+  console.log('[DEBUG] Runtime:', runtime.realname);
+  const result = await runtime.run('dist/cli.js tsx tests/hello.ts');
   assert.isTrue(result.status);
 }).disableTimeout();
